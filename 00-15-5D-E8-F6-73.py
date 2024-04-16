b1057444626a6b7c8feec2259df17586e5117a33
jonas638 <151398438+jonas638@users.noreply.github.com>
2024-04-16 20:11:45 +0200

    hello.py
    cleanup

--- 00-15-5D-E8-F6-73.py
+++ 00-15-5D-E8-F6-73.py
@@ -2,17 +2,11 @@ import os
 
 def create_hi_txt():
     try:
-        # Get the path to the desktop directory
+        
         desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-        
-        # Construct the full path to the hi.txt file
         hi_txt_path = os.path.join(desktop_path, "hi.txt")
-        
-        # Open the file in write mode and write the content "jonas was here"
         with open(hi_txt_path, "w") as file:
             file.write("jonas was here")
-        
-        print(f"File 'hi.txt' created successfully on the desktop with content 'jonas was here'.")
     except Exception as e:
         print("Error occurred:", e)
 
